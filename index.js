4dc8f08734ae16385ccdcfa0ae733cb2d87b6ef0
Roxolana <qweenlyana@gmail.com>
2016-09-06 11:34:41 +0000

    add new file

---
+++ index.js
@@ -0,0 +1,17 @@
+/*$(function() {
+
+	$(".top_nav a").mPageScroll2id({
+		offset: 100
+	});
+});*/
+
+$(document).ready(function(){
+    $(".top_nav").on("click","a",function (event){
+    event.preventDefault();
+    
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop:top}, 1500);
+    });
+});
+    
